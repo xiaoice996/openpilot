83774420035ed79008fc2a331db64919d0732768
Mr.one <mawei1991@gmail.com>
2026-06-06 21:45:59 +0800

    编辑launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# Skip onboarding on startup
+echo -n "2" > /data/params/d/HasAcceptedTerms
+echo -n "1.0" > /data/params/d/HasAcceptedTermsSP
+echo -n "0.2.0" > /data/params/d/CompletedTrainingVersion
 exec ./launch_chffrplus.sh
